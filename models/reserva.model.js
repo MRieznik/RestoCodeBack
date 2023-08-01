98a6c949a7259e6be1f15f400b91ba7171c6e5d9
Marcos Rieznik <m.rieznik17@gmail.com>
2023-08-01 14:25:58 -0300

    Anadiendo el schme de reserva

---
+++ models/reserva.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const reservaSchema = new Schema(
+  {
+    nombre: { type: String, 
+        required: true, 
+        max: 15, min: 3, 
+        trim: true },
+    apellido: { type: String, 
+        required: true, 
+        max: 20, 
+        min: 3, 
+        trim: true },
+    fecha: { type: String, 
+        required: true, 
+        max: 10, 
+        min: 10, 
+        trim: true },
+    hora: { type: String, 
+        required: true, 
+        max: 5, 
+        min: 5, 
+        trim: true },
+    personas: { type: Number, 
+        required: true, 
+        max: 30, 
+        min: 1, 
+        trim: true },
+    cometarios: { type: String, 
+        required: false, 
+        max: 50,
+        min: 5, 
+        trim: true },
+  },
+  { versionKey: false }
+);
+
+const ReservaModel = mongoose.model("reserva", reservaSchema);
+module.exports = ReservaModel;
