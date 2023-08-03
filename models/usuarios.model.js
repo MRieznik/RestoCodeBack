68a18fe97a39aeeaedfe48bf698f9004885cebae
Sergio Dominguez <sergiodominguez21sdd@gmail.com>
2023-08-02 22:36:23 -0300

    agregamos usuariosmodel

---
+++ models/usuarios.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const usuarioSchema = new Schema({
+
+    nombre: {
+        type: String,
+        required: true,
+        max: 30,
+        min: 3,
+        trim: true
+    },
+    apellido: {
+        type: String,
+        required: true,
+        max: 30,
+        min: 3,
+        trim: true
+    },
+    telefono: {
+        type: Number,
+        required: true,
+        unique: true,
+        max: 15,
+        min: 10,
+        trim: true
+
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 30,
+        min: 7,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 30,
+        min: 3,
+        trim: true
+    },
+    rol: {
+        type: String,
+        required: true,
+        max: 30,
+        min: 3,
+        trim: true
+    }
+
+}, { versionKey: false });
+
+const UsuariosModel = mongoose.model("usuarios", usuarioSchema);
+module.exports = UsuariosModel;
