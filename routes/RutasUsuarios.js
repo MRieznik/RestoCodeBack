a873957d08c79aa067a9bd08339b7a4329676523
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-08-02 23:02:30 -0300

    todas las rutas Usuarios

---
+++ routes/RutasUsuarios.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const UsuariosController = require('../controllers/usuariosController');
+
+router.post('/register', UsuariosController.register);
+router.post('/login', UsuariosController.loginUsuario);
+router.get('/usuarios', UsuariosController.getUsers);
+router.put("/updateUser/:id", UsuariosController.updateUser); 
+router.delete("/deleteUser/:id", UsuariosController.deleteUser);
+
+
+module.exports = router;
