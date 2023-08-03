275f73534dbc91c83e91feecda009778373681d2
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-08-02 23:01:55 -0300

    agrego usuariosController

---
+++ controllers/usuariosController.js
@@ -0,0 +1,113 @@
+const Usuarios = require("../models/usuarios.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//POST regitrar un usuario
+const register = async (req, res) => {
+  try {
+    const { nombre, apellido, email, telefono, password, rol } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+
+    const usuario = new Usuarios({
+      nombre,
+      apellido,
+      email,
+      telefono,
+      password: hash,
+      rol,
+    });
+    await usuario.save();
+    res.status(201).json("Usuario creado");
+  } catch (error) {
+    res.status(400).json(error, "usuario no Creado");
+  }
+};
+
+//POST loguear un usuario
+const loginUsuario = async (req, res) => {
+  const user = await Usuarios.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(400).json("Usuario y/o Password incorrecto");
+  }
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+
+  if (!match) {
+    return res.status(400).json("Usuario y/o Password incorrecto");
+  }
+
+  // generar el token
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      nombre: user.nombre,
+      apellido: user.apellido,
+      telefono: user.telefono,
+      rol: user.rol,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "1d" }
+  );
+
+  res.header("auth-token", token).json({
+    error: null,
+    data: { token },
+  });
+};
+
+//GET traer todos los usuarios
+const getUsers = async (req, res) => {
+  try {
+    const usuarios = await Usuarios.find();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(400).json("Usuarios no encontrados");
+  }
+};
+
+//PUT actualizar un usuario
+const updateUser = async (req, res) => { 
+    try {
+        const id = req.params.id;
+        const user = await UsuariosModel.findById(id);
+        if (user) {
+          user.nombre = req.body.nombre;
+          user.apellido = req.body.apellido;
+          user.telefono = req.body.telefono;          
+          const userActualizado = await user.save();
+          res.status(200).json("Usuario actualizado");
+          res.json(userActualizado);
+        } else {
+          res.status(404).json("Usuario no encontrado");
+        }
+      } catch (error) {
+        res.status(400).json("Usuario no actualizado");
+      }
+}
+
+ //DELETE borrar un usuario
+ const deleteUser = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const user = await UsuariosModel.findById(id);
+      if (user) {
+        await UsuariosModel.deleteOne({ _id: id });
+        res.status(200).json("Usuario eliminado");
+      } else {
+        res.status(404).json("Usuario no encontrado");
+      }
+    } catch (error) {
+      res.status(400).json("Usuario no eliminado");
+    }
+  };
+
+
+module.exports = {
+  register,
+  loginUsuario,
+  getUsers,
+  updateUser,
+  deleteUser
+};
