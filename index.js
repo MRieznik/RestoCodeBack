22893b344f5565933716587feea5a87d918a351c
Marcos Rieznik <m.rieznik17@gmail.com>
2023-08-08 09:43:10 -0300

    Cambiando la conexion a mongodb

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://restocode:DeqcYroq7H1X4XKS@restocode.e2zkoe4.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://restocode:DeqcYroq7H1X4XKS@restocode.e2zkoe4.mongodb.net/?retryWrites=true&w=majority";
+
+
+
+// const PORT = process.env.PORT || 3000;
 
 const initApp = async () => {
   try {
@@ -16,37 +19,29 @@ const initApp = async () => {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-      },
+      }
     });
 
     async function run() {
+      console.log("Iniciando la aplicación");
       try {
         await client.connect();
         await client.db("admin").command({ ping: 1 });
-        console.log(
-          "¡Se realizó un ping a tu implementación. Te has conectado exitosamente a MongoDB!"
-        );
+        console.log("Conectado con MongoDB");
       } finally {
         await client.close();
       }
     }
+    run().catch(console.dir);
 
-    await run(); // Espera a que se complete la conexión a MongoDB
 
-    // Ahora que la conexión a MongoDB está establecida, puedes iniciar la aplicación Express
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
-    });
-
-    console.log("Iniciando la aplicación");
   } catch (error) {
-    console.log("Error al iniciar la aplicación", error);
+    console.log("Error al iniciar la aplicación");
   }
 };
 
 initApp();
 
-
 app.use("/api", require("./routes/RutasReservas"));
 app.use("/api", require("./routes/RutasUsuarios"));
+
