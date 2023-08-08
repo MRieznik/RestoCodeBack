1814c63e32ac98ee05ac8b5f34be0cf138ad9984
Marcos Rieznik <m.rieznik17@gmail.com>
2023-08-08 09:20:59 -0300

    Probando nueva conexion

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
-const client = require("./db/mongodb");
 app.use(cors());
 require("dotenv").config();
-
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://restocode:DeqcYroq7H1X4XKS@restocode.e2zkoe4.mongodb.net/?retryWrites=true&w=majority";
 
 // const PORT = process.env.PORT || 3000;
 
@@ -16,8 +16,30 @@ const initApp = async () => {
     // app.listen(PORT, () => {
     //   // console.log(`Server running on port ${PORT}`);
     // });
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+          "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+      }
+    }
     console.log("Iniciando la aplicación");
-    await client();
   } catch (error) {
     console.log("Error al iniciar la aplicación");
   }
@@ -27,4 +49,3 @@ initApp();
 
 app.use("/api", require("./routes/RutasReservas"));
 app.use("/api", require("./routes/RutasUsuarios"));
-
