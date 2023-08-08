946297cb0300cc7926ba30a5429a180a8d160a3d
Marcos Rieznik <m.rieznik17@gmail.com>
2023-08-08 10:03:51 -0300

    Poniendo la contra

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://restocode:<password>@restocode.e2zkoe4.mongodb.net"
+const uri = "mongodb+srv://restocode:DeqcYroq7H1X4XKS@restocode.e2zkoe4.mongodb.net"
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
